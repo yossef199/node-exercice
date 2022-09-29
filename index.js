454d81ffd4ba294e1c82a3c35442837727c7d069
yossef199 <kassabiyossef@gmail.Com>
2022-09-29 22:15:34 +0200

    exercice node

---
+++ index.js
@@ -0,0 +1,5 @@
+const information = require('./information');
+console.log(`Hello, my name is ${information.name} from ${information.campus} `);
+
+import cowsay from 'cowsay';
+console.log(cowsay.campus('Hello!'));
---
+++ information.js
@@ -0,0 +1,11 @@
+const name = 'kassabi yossef';
+const campus = paris;
+
+console.log(name, campus );
+
+
+
+module.exports = {
+    name: name,
+    campus: campus,
+  };
